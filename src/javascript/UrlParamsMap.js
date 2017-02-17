fbf1e1ceb07408dafa6dc66b06a73fb14ea9fa6e
Marco Kellershoff <marco.kellershoff@gofeminin.de>
2017-02-17 14:30:53 +0100

    Make UrlParamsMap work with either `&amp;` or `&`

--- src/javascript/UrlParamsMap.js
+++ src/javascript/UrlParamsMap.js
@@ -1,7 +1,8 @@
 var UrlParamsMap = function(url) {
 	var tag = document.createElement('a');
 	tag.href = url;
-	var params_arr = tag.search.substring(1).split('&');
+	var splitter = (tag.search.indexOf('&amp;') !== -1) ? '&amp;' : '&';
+	var params_arr = tag.search.substring(1).split(splitter);
 	var params_map = {};
 	var kv;
 	var cb = function(param) {
